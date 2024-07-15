f25edccc263f226a5557f7a3ee3715b46a3a6f0d
p-miano <103672587+p-miano@users.noreply.github.com>
2024-07-15 16:20:49 -0300

    Added views, start, imports and layout files. Included Bootstrap and JQuery libraries. Added images and CSS code.

---
+++ Controllers/PieController.cs
@@ -0,0 +1,27 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+            //ViewBag.CurrentCategory = "Cheese cakes"; - Passes the data to the view using ViewBag
+            //return View(_pieRepository.AllPies); - Passes the data to the view using the model
+            // The following code passes the data to the view using the ViewModel
+            PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cakes");
+            return View(pieListViewModel);
+        }
+    }
+}
---
+++ ViewModels/PieListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BethanysPieShop.Models;
+
+namespace BethanysPieShop.ViewModels
+{
+    public class PieListViewModel
+    {
+        // This creates a model for the view that will contain an IEnumerable for the pies and a string for the current category
+        public IEnumerable<Pie> Pies { get; set; }
+        public string? CurrentCategory { get; set; }
+
+        public PieListViewModel(IEnumerable<Pie> pies, string? currentCategory)
+        {
+            Pies = pies;
+            CurrentCategory = currentCategory;
+        }
+    }
+}
