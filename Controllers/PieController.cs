819590ac5d53397898ea2ee3017bbf0f5a9fb0cb
p-miano <103672587+p-miano@users.noreply.github.com>
2024-07-20 13:43:20 -0300

    Added component to track how many items are in the shopping cart.

--- Controllers/PieController.cs
+++ Controllers/PieController.cs
@@ -15,13 +15,33 @@ namespace BethanysPieShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public IActionResult List()
+        //public IActionResult List()
+        //{
+        //    //ViewBag.CurrentCategory = "Cheese cakes"; - Passes the data to the view using ViewBag
+        //    //return View(_pieRepository.AllPies); - Passes the data to the view using the model
+        //    // The following code passes the data to the view using the ViewModel
+        //    PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "All pies");
+        //    return View(pieListViewModel);
+        //}
+
+        public ViewResult List(string category)
         {
-            //ViewBag.CurrentCategory = "Cheese cakes"; - Passes the data to the view using ViewBag
-            //return View(_pieRepository.AllPies); - Passes the data to the view using the model
-            // The following code passes the data to the view using the ViewModel
-            PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "All pies");
-            return View(pieListViewModel);
+            IEnumerable<Pie> pies;
+            string? currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PieListViewModel(pies, currentCategory));
         }
 
         public IActionResult Details(int id)
--- Models/BethanysPieShopDbContext.cs
+++ Models/BethanysPieShopDbContext.cs
@@ -4,10 +4,14 @@ namespace BethanysPieShop.Models
 {
     public class BethanysPieShopDbContext : DbContext
     {
+        // Inherits from DbContext, which is the base class in EF Core responsible for managing database connections and performing CRUD (Create, Read, Update, Delete) operations.
+
+        // Constructor that takes DbContextOptions as a parameter. This is the object that EF Core uses to configure the database connection. This includes specifying the database provider (SQL Server) and connection string, which are defined in the Program.cs file and read from the appsettings.json file.
         public BethanysPieShopDbContext(DbContextOptions<BethanysPieShopDbContext> options) : base(options)
         {
         }
 
+        // DbSet<TEntity> properties represent collections of entities that EF Core tracks and maps to database tables.
         public DbSet<Category> Categories { get; set; }
         public DbSet<Pie> Pies { get; set; }   
         public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
