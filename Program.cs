0b44dc39f4b6375c8ad69d9bc53f7abfae71d7b9
p-miano <103672587+p-miano@users.noreply.github.com>
2024-07-20 17:02:43 -0300

    Added popper.js library to make drop down Bootstrap menus work. Created Contact page.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args); // Loads the configuration fro
 
 // This block of code adds the interfaces created in the Models folder to the dependency injection container. 
 #region
+builder.Services.AddControllersWithViews(); // Adds MVC services to the services container
+
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IPieRepository, PieRepository>();
 // Using AddScoped means that the same instance of the service is used throughout the request.Both the interface and the implementation are added to the dependency injection container.
@@ -13,24 +15,29 @@ builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCa
 builder.Services.AddSession(); // Adds session support to the app
 builder.Services.AddHttpContextAccessor(); // Adds HttpContextAccessor to the services container
 
-builder.Services.AddControllersWithViews(); // Adds MVC services to the services container
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options => {
     options.UseSqlServer(
         builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
 }); // Adds the DbContext to the services container
 #endregion
+
 var app = builder.Build(); // Builds the app
 
 // This block of code sets up the request pipeline (middleware components)
 #region
-app.UseStaticFiles(); // For the wwwroot folder
-app.UseSession(); // Adds session support to the app
-
 if (app.Environment.IsDevelopment()) // Checks if the environment is Development
 {
     app.UseDeveloperExceptionPage(); // Shows detailed error information
 }
-app.MapDefaultControllerRoute(); // Adds a default route to the app {controller=Home}/{action=Index}/{id?}
+
+app.UseStaticFiles(); // For the wwwroot folder
+app.UseSession(); // Adds session support to the app
+
+// app.MapDefaultControllerRoute(); // Adds a default route to the app {controller=Home}/{action=Index}/{id?}
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 #endregion
 
 DbInitializer.Seed(app); // Seeds the database with data when the database is empty 
